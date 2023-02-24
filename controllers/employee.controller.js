047a0e3398a36b54984abb3ab3178a755326772b
kirran01 <Kirran-@live.com>
2023-02-24 13:59:15 -0400

    create employee

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,7 +1,21 @@
 const Employee = require("../models/Employee.model");
 
 const createEmployee = async (req, res) => {
-  res.send("create employee");
+  const { firstName, lastName, email, phone, image } = req.body;
+  try {
+    const newEmployee = await Employee.create({
+      firstName,
+      lastName,
+      email,
+      phone,
+      image,
+    });
+    if (newEmployee) {
+      res.send(newEmployee);
+    }
+  } catch (err) {
+    res.send(err);
+  }
 };
 
-module.exports={createEmployee}
+module.exports = { createEmployee };
