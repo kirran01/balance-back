1088487c22a869c342f9e911545a30144fa609d7
kirran01 <Kirran-@live.com>
2023-02-24 12:08:45 -0400

    auth

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv/config')
+const express=require('express')
+const mongoose=require('mongoose')
+
+const authRouter = require("./routes/auth.routes");
+
+const app=express();
+const PORT= process.env.PORT;
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+app.get('/',(req,res)=>{
+res.send('balance server up');
+})
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((connectRes) => {
+    console.log("//////////////////// PINTREST-CLONE //////////////////////");
+    console.log("connected to -->", connectRes.connections[0].name);
+    app.listen(PORT, () => {
+      console.log("Balance backend up on-->", +PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
